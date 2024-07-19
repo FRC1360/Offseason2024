260c65ebdad55838061e5eb293f9ea31754b1fe8
MadRobin13 <madrobin1340@gmail.com>
2024-07-18 20:09:50 -0400

    made new directory because issues woith old one

--- Offseason2024/src/main/java/frc/robot/Constants.java
+++ Offseason2024/src/main/java/frc/robot/Constants.java
@@ -7,7 +7,7 @@ package frc.robot;
 import com.pathplanner.lib.util.PIDConstants;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import swervelib.math.Matter;
+import swervelib.math.Matter; 
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
